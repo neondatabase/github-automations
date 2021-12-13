42cabb8de444b0686c281cda1d652cbea9b69f61
Stas Kelvich <stas.kelvich@gmail.com>
2021-12-14 02:27:05 +0300

    sync & tracked_in

---
+++ README.md
@@ -0,0 +1,44 @@
+# github-automations
+
+## Installation
+
+Tested with ruby 2.7.3 but expected to work all 2.x/3.x versions.
+
+In order for script to access github you need to:
+1. install github cli: https://github.com/cli/cli#installation
+1. give it api write access:
+    ```
+    gh auth login --scopes "write:org"
+    ```
+
+## Scripts
+
+`./sync_issues.rb` -- walks over all open issues in console and zeniths repos and add them to https://github.com/orgs/zenithdb/projects/6. Also sets `Tracked In` field if current issue is subtask of some other issue.
+
+## Relevant info
+
+Some guides on using API to manage projects: https://docs.github.com/en/issues/trying-out-the-new-projects-experience/using-the-api-to-manage-projects
+
+Issue-related events in github actions: https://docs.github.com/en/actions/learn-github-actions/events-that-trigger-workflows#issues
+
+It is handy to use https://docs.github.com/en/graphql/overview/explorer to check GraphQL queries.
+
+E.g. following query would print field id's in our project
+
+```graphql
+query { 
+  node(id: "PN_kwDOBKF3Cs1e-g") {
+      ... on ProjectNext {
+      fields(first: 20) {
+          nodes {
+          id
+          name
+          settings
+          }
+      }
+      }
+  }
+}
+```
+
+
---
+++ sync_issues.rb
@@ -0,0 +1,142 @@
+require 'json'
+
+def query(q)
+    JSON.load(`gh api graphql --header 'GraphQL-Features: projects_next_graphql' -f query='#{q}'`)
+end
+
+def get_issues(repo)
+  issues = []
+
+  # we can't get more than 100 issues at once, so parametrize query with cursor
+  query_builder = lambda do |condition| %Q{{
+      repository (owner: "zenithdb", name: "#{repo}") {
+        name
+        issues (first: 100, #{condition} states: [OPEN]) {
+          edges {
+            cursor
+          }
+          nodes {
+            id
+            number
+            title
+            timelineItems(first: 10, itemTypes: CROSS_REFERENCED_EVENT) {
+              nodes {
+                ... on CrossReferencedEvent {
+                  id
+                  source {
+                    ... on Issue {
+                      id
+                      title
+                      body
+                      number
+                    }
+                  }
+                }
+              }
+            }
+            body
+          }
+        }
+      }
+    }}
+  end
+
+  # get first 100 issues
+  resp = query(query_builder.call(''))
+  # and continue until result is empty
+  while resp['data']['repository']['issues']['nodes'].size > 0 do
+      issues += resp['data']['repository']['issues']['nodes']
+      cursor = resp['data']['repository']['issues']['edges'][-1]['cursor']
+      # get next 100 issues
+      resp = query(query_builder.call("after: \"#{cursor}\","))
+  end
+
+  # fill tracked_in for all issues: check if issue that referenced this one has
+  # markdown list with our issue number
+  # XXX: cross-repo issues?
+  issues = issues.map do |issue|
+    {
+      'id' => issue['id'],
+      'number' => issue['number'],
+      'title' => issue['title'],
+      'tracked_in' => issue['timelineItems']['nodes']
+        .filter { |timeline_item|
+          body = timeline_item['source']['body']
+          num = issue['number']
+          body && (body.include?("- [ ] ##{num}") || body.include?("- [x] #{num}"))
+        }
+        .map { |timeline_item|
+          src = timeline_item['source']
+          "#{src['title']} (https://github.com/zenithdb/#{repo}/issues/#{src['number']})"
+        }
+    }
+  end
+end
+
+def add_item(project_id, repo, issue)
+  puts issue
+
+  r1 = query(%Q{
+    mutation {
+      addProjectNextItem(input: {
+        projectId: "#{project_id}"
+        contentId: "#{issue['id']}"
+      }) {
+        projectNextItem { id }
+      }
+    }
+  })
+
+  project_item_id = r1['data']['addProjectNextItem']['projectNextItem']['id']
+
+
+  # gh api graphql -f query='
+  # query{
+  #   node(id: "PN_kwDOBKF3Cs1e-g") {
+  #     ... on ProjectNext {
+  #       fields(first: 20) {
+  #         nodes {
+  #           id
+  #           name
+  #           settings
+  #         }
+  #       }
+  #     }
+  #   }
+  # }'
+  tracked_field_id = 'MDE2OlByb2plY3ROZXh0RmllbGQ0ODg0OTM='
+
+  r2 = query(%Q{
+    mutation {
+      updateProjectNextItemField(input: {
+        projectId: "#{project_id}"
+        itemId: "#{project_item_id}"
+        fieldId: "#{tracked_field_id}"
+        value: "#{issue['tracked_in'].join(',')}"
+      }) {
+        projectNextItem { id }
+      }
+    }
+  })
+
+  puts r1
+  puts r2
+end
+
+# gh api graphql --header 'GraphQL-Features: projects_next_graphql' -f query='
+#   query {
+#     organization(login: "zenithdb"){
+#       projectNext(number: 6) {
+#           id
+#       }
+#     }
+#   }'
+project_id = 'PN_kwDOBKF3Cs1e-g'
+
+get_issues('zenith').each do |issue|
+  add_item(project_id, 'zenith', issue)
+end
+
+get_issues('console').each do |issue|
+  add_item(project_id, 'console', issue)
+end
