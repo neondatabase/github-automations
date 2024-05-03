5270b1e214d66b1f7edf46a971c99a84ce552178
Polina Semenova <polina.semenova112358@gmail.com>
2024-05-03 16:35:48 +0200

    only update field if the status field was changed

--- src/modules/status_last_updated_handler/config.ts
+++ src/modules/status_last_updated_handler/config.ts
@@ -1,5 +1,8 @@
 import {NEON_RELEASE_STATUS} from "../../shared/project_ids";
 
 export const config = {
-  [NEON_RELEASE_STATUS.projectId]: NEON_RELEASE_STATUS.statusLastUpdatedFieldId,
+  [NEON_RELEASE_STATUS.projectId]: {
+    statusLastUpdatedFieldId: NEON_RELEASE_STATUS.statusLastUpdatedFieldId,
+    statusFieldId: NEON_RELEASE_STATUS.statusFieldId
+  },
 }
--- src/modules/status_last_updated_handler/index.ts
+++ src/modules/status_last_updated_handler/index.ts
@@ -10,9 +10,18 @@ import {isDryRun} from "../../shared/utils";
 export const status_last_updated_handler = (app: Probot) => {
   app.on(["projects_v2_item.edited", "projects_v2_item.created"], async (context) => {
     // we use this event instead issue.edited because in this event we will get the project_node_id
-    logger("info", "status_last_updated_handler fired with payload", context.payload)
-    if (!(Object.keys(config)).includes(context.payload.projects_v2_item.project_node_id)) {
+    logger("info", "status_last_updated_handler fired with payload", context.payload);
+    const projectId = context.payload.projects_v2_item.project_node_id;
+    const statusFieldId = config[projectId].statusFieldId;
+    const lastUpdatedFieldId = config[projectId].statusLastUpdatedFieldId;
+    if (!(Object.keys(config)).includes(projectId) || !statusFieldId || !lastUpdatedFieldId) {
       logger("info", 'status_last_updated_handler skipped because not configured for the project')
+      return;
+    }
+
+    // @ts-ignore
+    if (context.payload.changes && context.payload.changes.fieldId !== statusFieldId) {
+      logger("info", 'status_last_updated_handler skipped because changes are not in status field')
       return;
     }
 
@@ -24,9 +33,9 @@ export const status_last_updated_handler = (app: Probot) => {
         });
       const updatedAt = (projectsItem as any).node.fieldValueByName.updatedAt;
       const params = {
-        project_id: context.payload.projects_v2_item.project_node_id,
+        project_id: projectId,
         project_item_id: context.payload.projects_v2_item.node_id,
-        date_field_id: config[context.payload.projects_v2_item.project_node_id],
+        date_field_id: lastUpdatedFieldId,
         value: updatedAt,
       };
       logger("info", "Update project v2 item with", params);
