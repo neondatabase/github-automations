d71bf4e758acbbe8f62b2536ce6591f23a09b46f
Polina Semenova <polina.semenova112358@gmail.com>
2025-04-16 17:30:19 +0200

    fix ts

--- src/backfill.ts
+++ src/backfill.ts
@@ -5,6 +5,7 @@ import {backfill_created_updated_deleted} from "./modules";
 // backfill scripts for automations, mostly run locally
 export = async (app: Probot) => {
 
+  // @ts-ignore
   const octo = await app.auth(process.env.GITHUB_INSTALLATION_ID);
   backfill_created_updated_deleted(octo);
 };
